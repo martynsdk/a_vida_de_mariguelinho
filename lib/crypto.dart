8fd6270721951f24d06a42657e162f6127ec29c3
martynsdk <martynsdk@gmail.com>
2022-06-16 19:16:35 -0300

    Crypto is Good

--- lib/crypto.dart
+++ lib/crypto.dart
@@ -1,6 +1,4 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'main.dart';
 import 'package:flutter/services.dart';
 
 final List<String> cryptoImageList = [
