c06024eb5a8e32f36bd1f16e6d2b94e738aeba0d
martynsdk <martynsdk@gmail.com>
2022-06-27 10:41:01 -0300

    Adjustments

--- lib/marighellos_pictures.dart
+++ lib/marighellos_pictures.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:localization/localization.dart';
 
 class MarighellosPictures extends StatefulWidget {
